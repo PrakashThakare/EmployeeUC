be685a6727609d46a5349bae89c6c4be7319ff1b
Shree Thakre <shreet365@gmail.com>
2022-12-19 17:05:31 +0530

    Update program.cs Calculate wage using switch case

--- EmployeeUC/Program.cs
+++ EmployeeUC/Program.cs
@@ -8,34 +8,35 @@ namespace EmployeeUC
 {
     internal class Program
     {
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        public const int EPM_RATE_PER_HOUR = 20;
         static void Main(string[] args)
         {
-            int IS_PART_TIME = 1;          //Constant
-            int IS_FULL_TIME = 2;          //Constants
-            int EPM_RATE_PER_HOUR = 20;    //Constants
-
-            int empHrs = 0;        //veriable
-            int empWage = 0;       //veriable
-
-            Random random = new Random();
-            int empCheck = random.Next(0, 3);
-
-            if (empCheck == IS_PART_TIME)
             {
-                empHrs = 4;
+                int empHrs = 0;        //veriable
+                int empWage = 0;       //veriable
+
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                empWage = empHrs * EPM_RATE_PER_HOUR;
+                Console.WriteLine("Emp Wage{0}", empWage);
+                Console.ReadLine();
             }
-            else if (empCheck == IS_FULL_TIME)
-            {
-                empHrs = 8;
-            }
-            else
-            {
-                empHrs = 0;
-            }
-            empWage = empHrs * EPM_RATE_PER_HOUR;
-            Console.WriteLine("Emp Wage is: {0}", empWage);
-            Console.ReadLine();
         }
+
     }
-    
 }
