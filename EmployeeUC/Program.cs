187ff4dc6c8b45619885b2c64993644921691456
Shree Thakre <shreet365@gmail.com>
2022-12-19 14:01:27 +0530

    Employee Attendance

--- EmployeeUC/Program.cs
+++ EmployeeUC/Program.cs
@@ -11,6 +11,21 @@ namespace EmployeeUC
         static void Main(string[] args)
         {
             Console.WriteLine("Welcom to EmployeeUC");
+            // Constants
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+
+
+            //Coputaion
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Not Present");
+            }
             Console.ReadLine();
         }
     }
